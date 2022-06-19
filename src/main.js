78eab9f3cb268f0daae9ca5dbdc388b4f813e835
Tyler Hartleb <tyler@hartleb.ca>
2022-06-18 19:05:47 -0600

    More amplify

--- src/main.js
+++ src/main.js
@@ -8,9 +8,14 @@ import App from "./App.vue";
 import router from './router';
 import smoothscroll from 'smoothscroll-polyfill';
 
+import Amplify from 'aws-amplify';  
+import '@aws-amplify/ui-vue';  
+import aws_exports from './aws-exports';  
 
- 
-// kick off the polyfill!
+Amplify.configure(aws_exports);  
+
+Vue.config.productionTip = false;
+
 smoothscroll.polyfill();
 
 const app = createApp(App);
