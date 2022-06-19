48961f72a700b4bbf387686c2a2dbdfaf38c2faa
Tyler Hartleb <tyler@hartleb.ca>
2022-06-18 19:33:10 -0600

    Amplify updates

--- src/main.js
+++ src/main.js
@@ -8,13 +8,12 @@ import App from "./App.vue";
 import router from './router';
 import smoothscroll from 'smoothscroll-polyfill';
 
-/*
-import Amplify from 'aws-amplify';  
-import '@aws-amplify/ui-vue';  
+
+import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';  
 
-//Amplify.configure(aws_exports); 
-*/
+Amplify.configure(aws_exports); 
+
 
 Vue.config.productionTip = false;
 
